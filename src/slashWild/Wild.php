0bb4c4df11491484ba3781029f14cb1f140a8b0d
eDroiid <edroiiid@gmail.com>
2016-12-30 00:43:58 -0800

    beta

---
+++ src/slashWild/Wild.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace LEET;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\level\{Level,Position,ChunkManager};
+use pocketmine\math\Vector3;
+
+class Wild extends PluginBase implements Listener {
+
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info("slashWild is enabled!");
+	}
+
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $param ) {
+		switch(strtolower($cmd->getName())) {
+
+			case "wild":
+				if($sender->hasPermission("slashWild.command.wild")) {
+					if($sender instanceof Player) {
+						$x = rand(1,350000);
+            $y = rand(1,256);
+						$z = rand(1,350000);
+						$sender->teleport($sender->getLevel()->getSafeSpawn(new Vector3($x, $y, $z)));
+						$sender->sendTip("[slashWild] You've been teleported somewhere wild!");
+						$sender->sendMessage("[slashWild] teleporting to: X-$x Z-$z");
+					}
+					else {
+						$sender->sendMessage("[slashWild] Only in-game!");
+					}
+				}
+				else {
+					$sender->sendMessage("[slashWild] You have no permission to use this command!");
+				}
+				return true;
+			break;
+
+		}
+	}
+	public function onDisable() {
+		$this->getLogger()->info("slashWild has been disabled!");
+	}
+
+}
