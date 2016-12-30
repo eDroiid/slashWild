42088013bf335954c156a4dfdfc78083bc0fa514
eDroid <edroiiid@gmail.com>
2016-12-29 20:03:19 -0800

    Update Wild.php

--- src/LEET/Wild.php
+++ src/LEET/Wild.php
@@ -12,33 +12,22 @@ use pocketmine\level\{Level,Position,ChunkManager};
 use pocketmine\math\Vector3;
 
 class Wild extends PluginBase implements Listener {
-	
+
 	public function onEnable() {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getLogger()->info("slashWild is enabled!");
 	}
-	
+
 	public function onCommand(CommandSender $sender, Command $cmd, $label, array $param ) {
 		switch(strtolower($cmd->getName())) {
-		
+
 			case "wild":
 				if($sender->hasPermission("slashWild.command.wild")) {
 					if($sender instanceof Player) {
-						
 						$x = rand(1,350000);
 						$z = rand(1,350000);
-						for($i = 0; $i < 128; $i++){
-							if(ChunkManager->getBlockIdAt($x, $i, $z) === 0){
-								if(ChunkManager->getBlockIdAt($x, $i+1, $z) === 0){
-									$y = $i;
-								}
-							}
-						}
-						if(!isset($y)){
-							$y = 128;	
-						}
-						$sender->teleport(new Position($x,$y,$z));
-						$sender->sendTip("WILD");
+						$sender->teleport($sender->getLevel()->getSafeSpawn(new Position($x,$y,$z)));
+						$sender->sendTip("[slashWild] You've been teleported somewhere wild!");
 						$sender->sendMessage("[slashWild] teleported to: X-".$x." Y-".$y." Z-".$z);
 					}
 					else {
@@ -50,12 +39,11 @@ class Wild extends PluginBase implements Listener {
 				}
 				return true;
 			break;
-		
+
 		}
 	}
-	
 	public function onDisable() {
 		$this->getLogger()->info("slashWild has been disabled!");
 	}
-	
+
 }
