7a1bb0cd0de9aa145aeea7a1d0f27bc2b02a6c59
eDroid <edroiiid@gmail.com>
2017-01-10 19:00:14 -0800

    Update Wild.php

--- src/slashWild/Wild.php
+++ src/slashWild/Wild.php
@@ -11,21 +11,19 @@ use pocketmine\Player;
 use pocketmine\level\{Level,Position,ChunkManager};
 use pocketmine\math\Vector3;
 
-class Wild extends PluginBase implements Listener {
+class Wild extends PluginBase {
 
 	public function onEnable() {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->getLogger()->info("slashWild is enabled!");
 	}
 
 	public function onCommand(CommandSender $sender, Command $cmd, $label, array $param ) {
-		switch(strtolower($cmd->getName())) {
-
+		switch(strtolower($cmd->getName())){
 			case "wild":
 				if($sender->hasPermission("slashWild.command.wild")) {
 					if($sender instanceof Player) {
 						$x = rand(1,350000);
-            $y = rand(1,256);
+            					$y = rand(1,256);
 						$z = rand(1,350000);
 						$sender->teleport($sender->getLevel()->getSafeSpawn(new Vector3($x, $y, $z)));
 						$sender->sendTip("[slashWild] You've been teleported somewhere wild!");
